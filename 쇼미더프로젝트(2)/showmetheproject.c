0da27fe01f64eb58bb7509dfd95a5ffbb0dee822
StarN2uk <108335640+StarN2uk@users.noreply.github.com>
2022-11-15 20:47:54 +0900

    Add files via upload

---
+++ 쇼미더프로젝트(2)/showmetheproject.c
@@ -0,0 +1,57 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#include<windows.h>
+int i;
+
+void Fileprinter(char score)
+{
+	FILE *fp = fopen("scoreFile.txt", "w");
+	fputc(score, fp);
+}
+
+void Theme(i)
+{
+	printf("###################가짜 PC방###################\n1. 룰렛 돌리기(코인 -> 5000원, 다이아몬드 -> 10000원, 꽝 -> 0원, 똥 -> -1000원)\n2. 가위 바위 보(이길 때만 1000원)\n3. 텔레파시 게임\n4. 남은 돈 확인하기&배고품 확인하기\n5. 음식 사기");
+	printf("\n당신이 원하는 게임 = ");
+	scanf("%d", &i); 
+}
+
+void GAME(int j)
+{
+	system("cls");
+	switch(j)
+	{
+		case 1:
+			Rullet();
+			break;
+		case 2:
+			break;
+		
+	}
+}
+
+void Rullet();
+{
+	system("cls");
+	srand(time(NULL));
+	printf("%d")
+	switch(rand())
+	{
+		case 1:
+			printf("")
+	}
+}
+
+
+
+
+
+
+int main()
+{
+	Fileprinter('0');
+	Theme(0);
+	GAME(i);
+	return 0;
+}
